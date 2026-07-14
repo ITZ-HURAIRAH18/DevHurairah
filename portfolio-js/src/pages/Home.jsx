defb070d533c7845e7dfde3a9809587f947ee6a3
ITZ-HURAIRAH18 <muhammadabuhurairah558@gmail.com>
2026-07-14 12:02:03 +0500

    AWS CI/CD done Successfully

--- portfolio-js/src/pages/Home.jsx
+++ portfolio-js/src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
               animation: "slideUp 0.8s 0.35s ease both",
             }}
           >
-            <span className="italic text-copper">Greatest of All</span> Hurairah
+            <span className="italic text-copper">Abu </span> Hurairah
           </span>
         </h1>
 
